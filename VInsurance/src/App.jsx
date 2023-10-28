f062cb36789ebf571cc3dc94fc38ed99594473b7
Shristi Pandey <goldiep04@gmail.com>
2023-10-28 19:35:03 +0530

    VInsurance

---
+++ VInsurance/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './Components/Login'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Dashboard from './Components/Dashboard'
+
+function App() {
+ 
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/adminlogin' element={<Login/>}></Route>
+      <Route path='/dashboard' element={<Dashboard/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
